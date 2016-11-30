49ea9b596b493c0f4cefadea9f7ca9b66f67462f
Keigo Ogawa <goog@brookbach.com>
2016-11-30 14:57:38 +0900

    exclude zprezto

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ for f in .??*
 do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".DS_Store" ] && continue
+    [ "$f" = ".zprezto" ] && continue
 
     ln -fsv "$HOME/dotfiles/$f" "$HOME"/"$f"
 done
